a6b2d88d80f99edbe7838054b54792211bb7fc55
Nicolas Moreau <mrnqoe@gmail.com>
2017-01-31 21:16:25 +0000

    fixed date

--- app.js
+++ app.js
@@ -15,11 +15,16 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query(`SELECT last_name, first_name, birthdate FROM famous_people WHERE last_name = '${input}'`, (err, result) => {
+  client.query('SELECT id, last_name, first_name, birthdate FROM famous_people WHERE last_name = $1::text', input, (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows);
+    console.log('searching...');
+    let res = result.rows;
+    console.log(`Found ${res.length} person(s) by the name '${input}':`);
+    res.forEach((i) => {
+      console.log(`- ${i.id} ${i.first_name} ${i.last_name}, born: ${i.birthdate.toDateString()} `);
+    })
     client.end();
   });
 });

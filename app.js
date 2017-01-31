02c24a65a2f3d298e796a524756439c0077161f3
Nicolas Moreau <mrnqoe@gmail.com>
2017-01-31 19:48:10 +0000

    Got the query going

---
+++ app.js
@@ -0,0 +1,25 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+var input = process.argv.slice(2);
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(`SELECT last_name, first_name, birthdate FROM famous_people WHERE last_name = '${input}'`, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows);
+    client.end();
+  });
+});
